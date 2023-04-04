d9fafb63b76d3651d3f971678489afa9ed849ffe
LJFrias <friaslukej@gmail.com>
2023-04-04 07:57:13 -0700

    added auth

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const session = require('express-session')
 const path = require('path')
 const usersRouter = require('./routers/users');
+const authRoutes = require('./routers/authRoutes')
+const messageRoutes = require('./routers/messageRoutes')
+
 
 const app = express()
 const port = process.env.PORT || 4001;
@@ -8,6 +12,9 @@ const port = process.env.PORT || 4001;
 app.use(express.static('public'))
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/', authRoutes)
+app.use('/', messageRoutes)
+app.use(session({secret: 'Your_Secret_Key', resave: true, saveUninitialized: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
